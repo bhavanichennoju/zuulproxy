057bdb550e272b1cd0043080a9d4bb4e6cc11d8a
Bhavani Chennoju <bhavanic@nbos.vinisoft.com>
2023-03-17 11:29:09 +0530

    zuul proxy

---
+++ diagnosis-service/src/test/java/com/javatechie/spring/rest/api/DiagnosisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiagnosisServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
